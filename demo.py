cab41aef03180c14c9d9b5465e5f508f7405d864
jchen80 <jychen835@gmail.com>
2022-08-04 08:28:25 -0700

    Add remaining demo repository files including files from conda and git demos

---
+++ demo.py
@@ -0,0 +1,7 @@
+def main():
+    a = 8
+    b = 4
+    print(f"This expression evaluates to true: {a} + {b} = {a + b}")
+    
+if __name__ == "__main__":
+    main()
---
+++ demo_module/math.py
@@ -0,0 +1,4 @@
+def my_math_function(a, b):
+    return a * b + (a ** b)
+
+_PRIVATE_CONSTANT = 3
